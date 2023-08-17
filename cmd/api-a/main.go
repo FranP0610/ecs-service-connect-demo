f20093d55ba8591156ae357befdfee2be7c7b4b3
FranP0610 <fran06.pe@gmail.com>
2023-08-17 01:48:00 -0500

    middleware added to both apis

--- cmd/api-a/main.go
+++ cmd/api-a/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"io"
+	"log"
 	"net/http"
 )
 
-const urlApiB = "http://api-b:8080/api-b/greetings"
+// const urlApiB = "http://api-b:8080/api-b/greetings"
+const urlApiB = "http://localhost:8080/api-b/greetings"
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Api A is working!"))
@@ -28,14 +30,21 @@ func callApiB(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	w.Write([]byte(body))
+	w.Write(body)
 }
-
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s - %s (%s)", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
 func main() {
 	r := mux.NewRouter()
+	//s.Use(logMiddleware)
 	s := r.PathPrefix("/api-a").Subrouter()
+	s.Use(logMiddleware)
 	s.HandleFunc("/health_check", healthCheck)
 	s.HandleFunc("/greetings", getGreetings)
 	s.HandleFunc("/api-b", callApiB)
-	http.ListenAndServe(":8080", s)
+	http.ListenAndServe(":8000", s)
 }
--- cmd/api-b/main.go
+++ cmd/api-b/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s - %s (%s)", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Api B is working!"))
 }
@@ -15,8 +22,9 @@ func getGreetings(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
+
 	s := r.PathPrefix("/api-b").Subrouter()
-	//r.PathPrefix("/api-a/")
+	s.Use(logMiddleware)
 	s.HandleFunc("/health_check", HealthCheckHandler)
 	s.HandleFunc("/greetings", getGreetings)
 	http.ListenAndServe(":8080", r)
